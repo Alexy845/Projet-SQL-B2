70b2c8922722ce47d1d47da65872379e310930f3
Alexy845 <alexy.daubresse@ynov.com>
2023-11-13 14:16:40 +0100

    exo 4 finis

--- student_answer/part_1/q4.js
+++ student_answer/part_1/q4.js
@@ -1,3 +1,15 @@
-const q4 = ``;
+const q4 = `
+SELECT
+    ability.ability_id AS Id,
+    ability.name AS Talent,
+    ability.description AS Description
+FROM
+    ability
+WHERE
+    ability.name LIKE '% %'
+    OR ability.name LIKE '%-%'
+ORDER BY
+    LENGTH(ability.description) ASC;
+`;
 
 module.exports = q4;
