a84059c20f0cb225f6b5ebf3bf2a349851f7571f
Alexy845 <alexy.daubresse@ynov.com>
2023-11-13 15:24:08 +0100

    exo 5 finis

--- student_answer/part_1/q5.js
+++ student_answer/part_1/q5.js
@@ -1,3 +1,17 @@
-const q5 = ``;
+const q5 = `
+SELECT
+    pokemon.pokedex_number AS 'N°',
+    pokemon.name AS 'Pokemon'
+FROM
+    pokemon
+INNER JOIN
+    pokemon_ability ON pokemon.pokemon_id = pokemon_ability.pokemon_id
+INNER JOIN
+    ability ON pokemon_ability.ability_id = ability.ability_id
+WHERE
+    ability.name = 'Coloforce'
+ORDER BY
+    pokemon.pokedex_number ASC;
+`;
 
 module.exports = q5;
