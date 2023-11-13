3bfbca7ffdc8ac816b8b959f0eb9a0f5d546e909
Alexy845 <alexy.daubresse@ynov.com>
2023-11-13 11:04:12 +0100

    exo 2 finis

--- student_answer/part_1/q2.js
+++ student_answer/part_1/q2.js
@@ -1,3 +1,13 @@
-const q2 = ``;
+const q2 = `
+SELECT
+    name AS Pokemon,
+    height || ' m' AS Taille,
+    weight || ' kg' AS Poids
+FROM
+    pokemon
+WHERE
+    height = (SELECT height FROM pokemon WHERE pokemon_id = 542)
+ORDER BY weight ASC
+`;
 
 module.exports = q2;
