48b6faf61617c3e24ca8e93c04a1804169a14b2f
Alexy845 <alexy.daubresse@ynov.com>
2023-11-13 14:14:25 +0100

    exo 3 finis

--- student_answer/part_1/q3.js
+++ student_answer/part_1/q3.js
@@ -1,3 +1,21 @@
-const q3 = ``;
+const q3 = `
+SELECT
+    move.name AS Nom,
+    move.category AS Catégorie,
+    move.power AS Puissance,
+    move.pp AS 'Point de pouvoir',
+    move.accuracy AS Précision,
+    move.description AS Description
+FROM
+    move
+INNER JOIN
+    type
+ON
+    move.type_id = type.type_id
+WHERE
+    type.name = 'Roche'
+ORDER BY
+    category ASC;
+`;
 
 module.exports = q3;
