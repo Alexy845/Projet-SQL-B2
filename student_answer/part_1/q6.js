95686b4a392f4bbc7c53419ebcb9aa0fc2a790b1
Alexy845 <alexy.daubresse@ynov.com>
2023-11-13 22:02:55 +0100

    exo 6 finis

--- student_answer/part_1/q6.js
+++ student_answer/part_1/q6.js
@@ -1,32 +1,18 @@
 const q6 = `
 SELECT
-pokemon.pokedex_number AS N°,
-pokemon.name AS Pokemon,
-ability.name AS Talent,
-ability.description AS 'Description talent'
+    pokemon.pokedex_number AS N°,
+    pokemon.name AS Pokemon,
+    ability.name AS Talent,
+    ability.description AS 'Description talent'
 FROM
-pokemon
+    pokemon
 INNER JOIN
-pokemon_ability ON pokemon.pokemon_id = pokemon_ability.pokemon_id
+    pokemon_ability ON pokemon.pokemon_id = pokemon_ability.pokemon_id
 INNER JOIN
-ability ON pokemon_ability.ability_id = ability.ability_id
-WHERE
-pokemon.pokedex_number IN (
-    SELECT
-        pokemon.pokedex_number
-    FROM
-        pokemon
-    INNER JOIN
-        pokemon_ability ON pokemon.pokemon_id = pokemon_ability.pokemon_id
-    INNER JOIN
-        ability ON pokemon_ability.ability_id = ability.ability_id
-    GROUP BY
-        pokemon.pokedex_number
-    HAVING
-        COUNT(*) = 1
-)
-ORDER BY
-pokemon.pokedex_number ASC;
+    ability ON pokemon_ability.ability_id = ability.ability_id
+GROUP BY ability.ability_id
+HAVING COUNT(ability.name) = 1
+ORDER BY pokemon.pokedex_number ASC;
 
 `;
 
--- student_answer/part_1/q7.js
+++ student_answer/part_1/q7.js
@@ -1,3 +1,20 @@
-const q7 = ``;
+const q7 = `
+SELECT
+    pokemon.pokedex_number AS N°,
+    pokemon.name AS 'Nom du pokemon',
+    pokemon_type.type_id AS 'Type 1',
+    COALESCE(type.name, 'N/A') AS 'Type 2'
+FROM
+    pokemon
+INNER JOIN
+    pokemon_type ON pokemon.pokemon_id = pokemon_type.pokemon_id and pokemon_type.slot = 1
+INNER JOIN
+    type ON pokemon_type.type_id = type.type_id
+INNER JOIN
+    pokemon_type AS pokemon_type2 ON pokemon.pokemon_id = pokemon_type2.pokemon_id and pokemon_type2.slot = 2
+INNER JOIN
+    type AS type2 ON pokemon_type2.type_id = type2.type_id
+ORDER BY
+    pokemon.pokedex_number ASC`;
 
 module.exports = q7;
