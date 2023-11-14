a1affada857e94db95b89552d7faabe8f2d090be
Alexy845 <alexy.daubresse@ynov.com>
2023-11-14 14:37:37 +0100

    exo 12 finis

--- student_answer/part_1/q12.js
+++ student_answer/part_1/q12.js
@@ -1,3 +1,22 @@
-const q12 = ``;
+const q12 = `
+SELECT
+    pokemon.pokedex_number AS N°,
+    pokemon.name AS 'Nom du pokemon',
+    ability.name AS 'Nom du talent'
+FROM
+    pokemon
+INNER JOIN
+    pokemon_type ON pokemon.pokemon_id = pokemon_type.pokemon_id
+INNER JOIN
+    type ON pokemon_type.type_id = type.type_id
+INNER JOIN
+    pokemon_ability ON pokemon.pokemon_id = pokemon_ability.pokemon_id
+INNER JOIN
+    ability ON pokemon_ability.ability_id = ability.ability_id
+WHERE
+    type.name = 'Acier' and is_hidden = 1
+ORDER BY
+    pokemon.pokedex_number;
+`;
 
 module.exports = q12;
