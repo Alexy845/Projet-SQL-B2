f41923e3932bb3fe8540c394a95c1445cedde092
Alexy845 <alexy.daubresse@ynov.com>
2023-11-14 15:03:07 +0100

    exo 13 finis

--- student_answer/part_1/q13.js
+++ student_answer/part_1/q13.js
@@ -1,3 +1,25 @@
-const q13 = ``;
+const q13 = `
+SELECT pokemon.name AS 'Nom du pokemon',
+    COUNT(*) AS 'Nombre capacité avec le même type'
+FROM
+    pokemon
+INNER JOIN
+    pokemon_type pt ON pokemon.pokemon_id = pt.pokemon_id
+INNER JOIN
+    type ON type.type_id = pt.type_id
+INNER JOIN
+    move ON type.type_id = move.type_id
+INNER JOIN
+    pokemon_move ON pokemon_move.move_id = move.move_id
+WHERE
+    pokemon.pokemon_id = pokemon_move.pokemon_id
+GROUP BY
+    pokemon.name
+ORDER BY
+    COUNT(*) DESC,
+    pokemon.name ASC
+LIMIT 10;
+
+`;
 
 module.exports = q13;
