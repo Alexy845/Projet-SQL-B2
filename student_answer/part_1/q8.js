9980cc0b047cf9235a10aa584849db743c9a54b2
Alexy845 <alexy.daubresse@ynov.com>
2023-11-14 09:51:13 +0100

    exo 8 finis

--- student_answer/part_1/q8.js
+++ student_answer/part_1/q8.js
@@ -1,3 +1,19 @@
-const q8 = ``;
+const q8 = `
+SELECT
+    type.name AS 'Nom du type',
+    COUNT(pokemon.pokemon_id) AS 'Nombre de pokemon',
+    SUM(CASE WHEN pt.slot = 1 THEN 1 ELSE NULL END) AS 'Nombre de pokemon avec le type slot 1',
+    SUM(CASE WHEN pt.slot = 2 THEN 1 ELSE NULL END) AS 'Nombre de pokemon avec le type slot 2'
+FROM
+    type
+LEFT JOIN
+    pokemon_type pt ON type.type_id = pt.type_id
+LEFT JOIN
+    pokemon ON pt.pokemon_id = pokemon.pokemon_id
+GROUP BY
+    type.type_id
+ORDER BY
+    "Nombre de pokemon" DESC, "Nombre de pokemon avec le type slot 1" DESC;
+`;
 
 module.exports = q8;
