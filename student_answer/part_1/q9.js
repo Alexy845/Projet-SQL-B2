972ffb51be985850fbb079ca32decf17cfeb9994
Alexy845 <alexy.daubresse@ynov.com>
2023-11-14 10:24:09 +0100

    exo 9 finis

--- student_answer/part_1/q9.js
+++ student_answer/part_1/q9.js
@@ -1,3 +1,19 @@
-const q9 = ``;
-
+const q9 = `
+SELECT
+    type.name AS Type,
+    move.name AS Capacité,
+    move.power AS Puissance,
+    move.accuracy AS Précision,
+    move.description AS Description
+FROM
+    move
+INNER JOIN
+    type ON move.type_id = type.type_id
+WHERE
+    move.power > 100
+    AND move.accuracy >= 90
+    AND move.description NOT LIKE '% tour %'
+ORDER BY
+    move.power DESC;
+`;
 module.exports = q9;
