61037f8c76da94d28bd81000d92373c7fcb2e47d
Alexy845 <alexy.daubresse@ynov.com>
2023-11-14 14:15:30 +0100

    exo 11 finis

--- student_answer/part_1/q11.js
+++ student_answer/part_1/q11.js
@@ -1,3 +1,18 @@
-const q11 = ``;
+const q11 = `
+SELECT
+    pokemon.name AS Pokemon,
+    COUNT(move.move_id) AS 'Nombre de capacités'
+FROM
+    pokemon
+LEFT JOIN
+    pokemon_move ON pokemon.pokemon_id = pokemon_move.pokemon_id
+LEFT JOIN
+    move ON pokemon_move.move_id = move.move_id
+GROUP BY
+    pokemon.name
+ORDER BY
+    COUNT(move.move_id) DESC
+LIMIT 1;
+`;
 
 module.exports = q11;
